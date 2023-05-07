f2223b6ac95547daec194be8cadd6687c28767b9
Kostia1023 <kostia1023@gmail.com>
2023-05-07 17:27:20 +0300

    add enemy afk

--- Assets/Script/Enemy/EnemyMovement.cs
+++ Assets/Script/Enemy/EnemyMovement.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public class EnemyMovement : MoveController
 {
-    public float radius = 10;
+    public float radius = 2f; // Радіус квадрата руху
     public string targetTag = "Player";
+    public float minPauseTime = 3f; // Мінімальний час паузи після досягнення точки
+    public float maxPauseTime = 8f; // Максимальний час паузи після досягнення точки
 
     public CharacterController characterController;
 
     private GameObject target;
+    private Vector3 originalPosition;
+    private Vector3 randomDestination;
+    private bool isMoving;
+    private bool isPaused;
+    private float pauseTimer;
 
     private void Start()
     {
         target = null;
+        originalPosition = transform.position;
+        SetRandomDestination();
+
+        if (target == null)
+        {
+            isMoving = true;
+        }
     }
 
     private void Update()
     {
         if (target == null)
         {
-            // Find player with target tag
-            GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
-            foreach (GameObject potentialTarget in targets)
-            {
-                float distance = Vector3.Distance(transform.position, potentialTarget.transform.position);
-                if (distance <= radius)
-                {
-                    target = potentialTarget;
-                    break;
-                }
-            }
+            FindPlayerTarget();
         }
         else
         {
@@ -38,13 +42,70 @@ public class EnemyMovement : MoveController
             if (distance > radius)
             {
                 target = null;
+                isMoving = true;
             }
             else
             {
-                // Move towards target
-                Vector3 direction = (target.transform.position - transform.position).normalized;
-                characterController.Move(getSpeed(direction));
+                MoveTowardsTarget();
             }
         }
+
+        if (isMoving)
+        {
+            MoveWithinSquare();
+        }
+        else if (isPaused)
+        {
+            pauseTimer -= Time.deltaTime;
+            if (pauseTimer <= 0f)
+            {
+                isMoving = true;
+                isPaused = false;
+                SetRandomDestination();
+            }
+        }
+    }
+
+    private void FindPlayerTarget()
+    {
+        GameObject[] targets = GameObject.FindGameObjectsWithTag(targetTag);
+        foreach (GameObject potentialTarget in targets)
+        {
+            float distance = Vector3.Distance(transform.position, potentialTarget.transform.position);
+            if (distance <= radius)
+            {
+                target = potentialTarget;
+                break;
+            }
+        }
+    }
+
+    private void MoveTowardsTarget()
+    {
+        Vector3 direction = (target.transform.position - transform.position).normalized;
+        transform.LookAt(target.transform);
+        characterController.Move(getSpeed(direction));
+    }
+
+    private void MoveWithinSquare()
+    {
+        if (Vector3.Distance(transform.position, randomDestination) <= 0.1f)
+        {
+            isMoving = false;
+            isPaused = true;
+            pauseTimer = Random.Range(minPauseTime, maxPauseTime);
+        }
+        else
+        {
+            Vector3 direction = (randomDestination - transform.position).normalized;
+            characterController.Move(getSpeed(direction*0.4f));
+        }
+    }
+
+    private void SetRandomDestination()
+    {
+        float randomX = Random.Range(originalPosition.x - radius, originalPosition.x + radius);
+        float randomZ = Random.Range(originalPosition.z - radius, originalPosition.z + radius);
+        randomDestination = new Vector3(randomX, originalPosition.y, randomZ);
     }
 }
